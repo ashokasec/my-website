1f3a788aca381ca4f912b4d50015a683edf1c8c3
Shivam Gupta <ashokasec@proton.me>
2024-06-04 11:35:27 +0530

    Updated Linkedin Link

--- src/links.ts
+++ src/links.ts
@@ -1,6 +1,6 @@
 export const socialMedia = {
     instagram: "https://instagram.com/ashokasec",
-    linkedin: "https://linkedin.com/in/ashokasec",
+    linkedin: "https://linkedin.com/in/ashokasecx",
     twitter: "https://twitter.com/ashokasec",
     github: "https://github.com/ashokasec"
 }
